f1ec8cc822366a92c5f16113f991b45c2b70d869
Seth Jeffery <seth@quipper.com>
2017-04-29 22:19:12 +0100

    add some comments

--- src/components/Player.js
+++ src/components/Player.js
@@ -50,6 +50,7 @@ export default class Player extends Component {
     const body = this.body
     switch(e.which) {
       case SPACE:
+        // SPACE bar jumps! (If we are on the ground, that is.)
         if(this.onGround) {
           this.onGround = 0
           Matter.Body.applyForce(
@@ -70,19 +71,33 @@ export default class Player extends Component {
 
   update = () => {
     const body = this.body
+
+    // Reduce onGround tracker from 10 back to 0. After 10 frames without
+    // colliding with the ground, we can assume we are "in the air".
     this.onGround = Math.max((this.onGround || 0) - 1, 0)
+
+    // Perform movement
     this.move(body)
     this.moveStage()
     this.props.updatePlayer({ position: body.position, angle: body.angle })
+
+    // At bottom of screen, fall through by filtering out collisions
+    // and disallowing the player to jump any further
     if(body.position.y > 580) {
       body.collisionFilter = { mask: 1 }
+      this.onGround = 0
     }
+
+    // After falling, game over!
     if(body.position.y > 800) {
       this.props.endGame()
     }
   }
 
   moveStage = () => {
+    // Move the stage down by a speed factor dependent on:
+    //  - If player approaches top of screen, stage moves to compensate
+    //  - As level increases, stage moves faster
     const body = this.body
     const { engine, store } = this.context
     const stageLevel = store.getState().app.level.stageLevel
@@ -92,6 +107,9 @@ export default class Player extends Component {
   }
 
   move = body => {
+    // Apply rolling force to player if LEFT or RIGHT keys are held down.
+    // If we are on the ground, we can roll faster.
+    // If in the air, we can turn a little but not too much.
     if (this.isKeyDown(CURSOR_LEFT)) {
       Matter.Body.applyForce(
         body,
@@ -109,15 +127,21 @@ export default class Player extends Component {
   }
 
   resting = ({ pairs }) => {
+    // If player is colliding with a level platform and is ABOVE it, then we are "on the ground"
+    // meaning we can jump, and can move left/right faster than while in air.
     if (pairs.some(pair => {
       return (pair.bodyA === this.body && pair.bodyA.position.y < pair.bodyB.position.y) ||
         (pair.bodyB === this.body && pair.bodyB.position.y < pair.bodyA.position.y)
     })) {
+      // onGround is a numeric that increases up to a value of 10. This is because while
+      // we are rolling on the ground there are moments when we are "in the air"
+      // but we should only count after a reasonable length of time (10 frames).
       this.onGround = Math.min((this.onGround || 0) + 3, 10)
     }
   }
 
   componentDidMount() {
+    // Listen for game loop events, collision events, and key presses.
     Matter.Events.on(this.context.engine, 'afterUpdate', this.update)
     Matter.Events.on(this.context.engine, 'collisionStart', this.resting)
     Matter.Events.on(this.context.engine, 'collisionActive', this.resting)
@@ -125,6 +149,7 @@ export default class Player extends Component {
   }
 
   componentWillUnmount() {
+    // Release events and Matter body object.
     Matter.Events.off(this.context.engine, 'afterUpdate', this.update);
     Matter.Events.off(this.context.engine, 'collisionStart', this.resting)
     Matter.Events.off(this.context.engine, 'collisionActive', this.resting)
